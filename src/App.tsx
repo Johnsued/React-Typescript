cf4585acf3a1419c35a3efcc4ca9b9de03a2d6e7
John Soares <johnsued25@gmail.com>
2022-07-02 11:21:35 -0300

    firt commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulario from './components/Formularios';
+import Lista from './components/Lista';
+
+function App() {
+  return (
+    <div className="App">
+       <Formulario />
+       <Lista />
+    </div>
+  );
+}
+
+export default App;
