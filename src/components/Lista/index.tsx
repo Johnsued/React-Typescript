3c4663b63f550d561760e9015ee06c6c010e9188
John Soares <johnsued25@gmail.com>
2022-07-06 23:23:32 -0300

    IDK

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,9 +1,9 @@
-import react from 'react';
+import react, { useState } from 'react';
 import Item from './item';
 import style from'./Lista.module.scss';
 
 function Lista() {
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:00:00'
     }, {
@@ -12,10 +12,13 @@ function Lista() {
     }, {
         tarefa: 'TypeScript',
         tempo: '00:30:00'
-    }]
+    }]);
     return (
         <aside className={style.listaTarefas}>
-            <h2> Estudos do dia</h2>
+            <h2 onClick={() => {
+                console.log("h2 clicado", tarefas);
+                setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}]) 
+            }}> Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
                    <Item key={index}
