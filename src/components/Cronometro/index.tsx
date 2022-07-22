10ec029b43d7b7dd7c42c607cd5beb81b95de8c1
John Soares <johnsued25@gmail.com>
2022-07-22 17:40:20 -0300

    after created the clock

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,26 +1,29 @@
 import Botao from "../Botao";
-import Relogio from './Relógio'
-import style from './cronometro.module.scss'
+import Relogio from './Relógio';
+import style from './cronometro.module.scss';
 import { tempoParaSegundos } from "../../common/utils/time";
 import { ITarefa } from "../../types/tarefa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
     selecionado: ITarefa | undefined
 };
 
 export default function Cronometro ({selecionado}: Props) {
-    const [tempo, setTempo] = useState<Number>();
-    if(selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado?.tempo));
-    }
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+},[selecionado])
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha o card e 
             inicie o cronometro</p>
-            Tempo: {tempo}
             <div className={style.relogioWrapper}>
-                <Relogio />
+                <Relogio tempo={tempo}/>
             </div>
             <Botao>
                 Começar!
